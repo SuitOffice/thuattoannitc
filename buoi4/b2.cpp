5004035f1754fe785b9d08d367eca05b4ac87e1a
SuitOffice <suoffice.it@gmail.com>
2024-05-04 17:32:08 +0700

    add file

--- buoi4/b2.cpp
+++ buoi4/b2.cpp
@@ -1,42 +1,32 @@
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int a[10001], n, stop = 0;
+int a[100], n, stop = 0;
 void khoitao(){
-    for(int i = 1; i <= n; i++) a[i] = i;
+    for(int i = 0; i < n; i++) a[i] = 0;
 }
 void xuat(){
-    for(int i = 1; i <= n; i++) cout << a[i];
+    for(int i = 0; i < n; i++) cout << a[i];
     cout << endl;
 }
 void sinh(){
-  int i = n - 1;
-  // tim a[i] ko tang dan a[i] < a[i+1]
-  while(i>=1&&a[i] > a[i+1]) i--;
-  if(i==0) stop = 1;
-  else{
-    //tim phan tu nho nhat lon hon a[i]
-    int k = n;
-    while(a[i]>a[k]) k--;
-    swap(a[i],a[k]);
-    //sap xep lai cac han tu dang sau theo thu tu tang dan
-    int c = n, l = i + 1;
-    while(l<c) {
-      swap(a[l], a[c]);
-      l++;
-      c--;
+    int i = n - 1;
+    while(i>=0&&a[i]==1){
+        a[i] = 0;
+        i--;
     }
-    // hoac dung reverse(a+i+1,a+n+1)
-  }
+    if(i==-1) stop = 1;
+    else{
+        a[i] = 1;
+    }
 }
 void chuongtrinh(){
-  while(stop == 0){
-    xuat();
-    sinh();
-  }
+    while(stop==0){
+        xuat();
+        sinh();
+    }
 }
 int main(){
-  cin >> n;
-  khoitao();
-  chuongtrinh();
+    cin >> n;
+    khoitao();
+    chuongtrinh();
 }
